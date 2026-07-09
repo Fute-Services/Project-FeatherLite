94da310855c7a3cf28a78691dd50ed31fc6aa2f9
Fute Services <webdev1@futeservices.com>
2026-07-09 14:47:20 +0530

    Replace Location page image with embedded panorama iframe and logo overlay

--- client/src/pages/Location/Location.tsx
+++ client/src/pages/Location/Location.tsx
@@ -1,12 +1,32 @@
-import bgImage from "../../assets/LocationPage/Copy of Featherlite Signature Aerial Shot_01 day (1).jpg";
+// import bgImage from "../../assets/LocationPage/Copy of Featherlite Signature Aerial Shot_01 day (1).jpg";
+import logo from "../../assets/logo.png";
+
 function Location() {
   return (
-    <div className="w-full h-screen overflow-hidden bg-neutral-900">
-      <img
+    <div className="relative w-full h-screen">
+      {/* <img
         src={bgImage}
         alt="Location"
-        className="w-full h-full object-contain lg:object-cover"
+        className="w-full h-full object-cover"
+      /> */}
+      <iframe
+        src="https://futeservices.com/26-27/Featherlight/index.html"
+        title="Location Panorama"
+        className="w-full h-full border-none"
+        allow="accelerometer; gyroscope; magnetometer; vr"
+        allowFullScreen
       />
+
+      {/* Top Right Logo */}
+      <div className="absolute top-4 right-4 md:top-6 md:right-10 z-20 pointer-events-none">
+        {/* Soft shadow background for logo visibility */}
+        <div className="absolute inset-[-40px] bg-sky-800/30 blur-[40px] rounded-full -z-10" />
+        <img
+          src={logo}
+          alt="Logo"
+          className="relative h-14 md:h-24 w-auto object-contain drop-shadow-2xl"
+        />
+      </div>
     </div>
   );
 }
